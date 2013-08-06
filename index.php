56c5a3e842b95c1690153aaca5568427bb354809
jkagan <kahanjoseph@gmail.com>
2013-08-05 20:49:50 -0400

    regular and associative arrays

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
+  
+  $myarray = array('steve', 'joe','stan');
+  //example of regular array
 
-  print_r($_REQUEST);
+  print_r($_myarray);
+
+  $associative_array = array ('name1' => 'joe');
+
+  print_r($accociative_array);
   
   
 
