c432d81281b55f11bb303e8385b6600ecbf95c42
jkagan <kahanjoseph@gmail.com>
2013-08-05 20:54:31 -0400

    fixed mistake

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
   $myarray = array('steve', 'joe','stan');
   //example of regular array
 
-  print_r($_myarray);
+  print_r($myarray);
 
   $associative_array = array ('name1' => 'joe');
 
-  print_r($accociative_array);
+  print_r($associative_array);
   
   
 
