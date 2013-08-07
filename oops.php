6421bbccbcbed5b8dbf0a3caf01cfae14afe5b07
jkagan <kahanjoseph@gmail.com>
2013-08-07 13:29:13 -0400

    disabled broken class

--- oops.php
+++ oops.php
@@ -1,14 +1,16 @@
 <?php
 
+    //$obj =new messagePrint ('My Message');
+  
     $obj =new addItUp(4,5);
     $obj =new addItUp(5,8);
   
-  class messagePrint{
-    function __construct($message){
-      echo $message;
-      echo 'some other message';
-    }
-  }
+  //class messagePrint{
+    //function __construct($message){
+      //echo $message;
+      //echo 'some other message';
+    //}
+  //}
   
   class addItUp {
   
