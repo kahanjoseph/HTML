5057734424dae0985af2483ded83a11df5aced56
jkagan <kahanjoseph@gmail.com>
2013-08-07 12:56:11 -0400

    new oop program??

---
+++ oops.php
@@ -0,0 +1,28 @@
+<?php
+
+    $obj =new addItUp(4,5);
+    $obj =new addItUp(5,8);
+  
+  class messagePrint{
+    function __construct($message){
+      echo $message;
+      echo 'some other message';
+    }
+  }
+  
+  class addItUp {
+  
+    function __construct($a,$b){
+    $c = $a +$b ;
+ 
+      echo $a, '+', $b ,'=', $c; 
+    }
+  } 
+
+
+
+
+
+
+
+?>
